499a4528334e0f301603bc2aaaae688c68d36ff7
Naoise Ryan <naoisethecool@gmail.com>
2024-02-08 12:03:07 +0000

    Setup assignments class

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+class newAssignment {
+    constructor(title, subject, description, dueDate) {
+        this.title = title;
+        this.subject = subject;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+}
+
+const assignmentsArray = [];
+
+assignmentsArray.push("Test");
